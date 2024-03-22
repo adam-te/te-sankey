b3060358321399c1afd2eefc7ce8d3f1c52be62a
adam <adam@thousandeyes.com>
2024-03-21 20:48:49 -0500

    stash

--- src/cloudFlowUtils/sortToMinimizeLinkCrossings.ts
+++ src/cloudFlowUtils/sortToMinimizeLinkCrossings.ts
@@ -17,6 +17,38 @@ export function sortToMinimizeLinkCrossings({
       const column = columns[j];
       const nextColumn = columns[j + 1];
       sortNodesByBarycenter(column, nextColumn);
+
+      const isLastIteration = i === numberOfIterations - 1;
+      if (isLastIteration) {
+        sortLinksByTargetIdx(column, nextColumn);
+      }
+    }
+  }
+
+  // Don't cross own node links
+  function sortLinksByTargetIdx(
+    column: SankeyColumn,
+    nextColumn: SankeyColumn
+  ) {
+    const nodeIdToNextColumnIdx = new Map(
+      nextColumn.nodes.map((node, index) => [node.id, index])
+    );
+
+    for (const node of column.nodes) {
+      node.sourceLinks.sort((a, b) => {
+        const aTargetRowIdx = nodeIdToNextColumnIdx.get(a.target.id);
+        const bTargetRowIdx = nodeIdToNextColumnIdx.get(b.target.id);
+        if (aTargetRowIdx == null && bTargetRowIdx == null) {
+          return 0;
+        }
+        if (aTargetRowIdx == null) {
+          return 1;
+        }
+        if (bTargetRowIdx == null) {
+          return -1;
+        }
+        return aTargetRowIdx - bTargetRowIdx;
+      });
     }
   }
 
@@ -49,4 +81,9 @@ export function sortToMinimizeLinkCrossings({
         : null;
     }
   }
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it("TODO", () => {});
 }
